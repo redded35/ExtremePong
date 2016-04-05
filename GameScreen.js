f17e8c8b724396779cac53e545cedd547e1ec10a
redded35 <benthebat@missionbits-MacBook-Air.local>
2016-04-04 18:04:10 -0700

    basket auto move left right. shoot ball, move ball left right. also we got it to move at funny speed and ball hit basket, ball hit edge.
    basket auto move left right. shoot ball, move ball left right. also we
    got it to move at funny speed and ball hit basket, ball hit edge.

--- GameScreen.js
+++ GameScreen.js
@@ -1,3 +1,4 @@
+var isShooting = false;
 //creates a GameScreen object
 var GameScreen = {
     
@@ -6,8 +7,11 @@ var GameScreen = {
     preload : function() {
         //loads an image named 'logo'
         game.load.image('logo', 'assets/images/basketball.png');
-        
-        },
+        game.load.image('hoop', '/assets/images/basketball-net.jpg');
+        game.load.image('bg', '/assets/images/basketball-court-backgrounds1.jpg');
+        //loads an image named 'start'
+        game.load.image('start', '/assets/images/start.png');
+    },
     
     //the create method is run after the preload method
     //it is where we set up the basics of the game, essentially what it will look like when we start the game
@@ -15,42 +19,70 @@ var GameScreen = {
 //makes the background color of the whole screen black
         game.stage.backgroundColor = '#000000';
         
+        this.bg = game.add.image(0,0, 'bg');
+        this.bg.width = 640;
+        this.bg.height = 480;
+        
         //starts the physics system for the game
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
         //creates a variable that handles the arrow keys
         this.cursor = game.input.keyboard.createCursorKeys();
         
+        this.hoop = game.add.sprite(game.world.centerX - 100, 0, 'hoop');
+        this.hoop.scale.x = 0.50;
+        this.hoop.scale.y = 0.50;
+        game.physics.arcade.enable(this.hoop);
+        this.hoop.body.immovable = true;
+        this.hoop.body.velocity.x = 6000;
+        this.hoop.body.collideWorldBounds = true;
+        this.hoop.body.bounce.x = 1;
+        
         //creates a sprite with the 'logo' image at (200, 400) and assigns it to a variable
         this.mc = game.add.sprite(50, 4308, 'logo');
-        this.mc.scale.x = 0.25;
-        this.mc.scale.y = 0.25;
+        this.mc.scale.x = 0.16;
+        this.mc.scale.y = 0.16;
         //enables the physics system for the mc
         game.physics.arcade.enable(this.mc);
         
-        //makes sure the mc can't be moved by other sprites
-        this.mc.body.immovable = true;
-        
         //make it so the mc can't leave the screen
         this.mc.body.collideWorldBounds = true;
-
+        
     },
     
     //function that is called 60 times per second
     //where we put the logic of the game
     update: function() {
-    
-        //if the right arrow is pressed, move to the right
-        if (this.cursor.right.isDown) {
-            this.mc.body.velocity.x = 350;
-        } else if (this.cursor.left.isDown) { //if the left arrow is pressed, move to the left
-            this.mc.body.velocity.x = -350;
-        } else { //if no arrow keys are being pressed, stop moving
-            this.mc.body.velocity.x = 0;
-            this.mc.body.velocity.y = 0;
+        
+        game.physics.arcade.collide(this.hoop, this.mc, this.hit, null, this);
+        if(this.mc.y == 0){
+            console.log("test");
+            this.mc.x = 50;
+            this.mc.y = 4308;
+            isShooting = false;
         }
-    
-    }
 
+        //if the right arrow is pressed, move to the right        
+        if (this.cursor.up.isDown) {
+            isShooting = true;
+            this.mc.body.velocity.x = 0;
+            this.mc.body.velocity.y = -4555;         
+        }else{
+            if (this.cursor.right.isDown && isShooting == false) {
+                this.mc.body.velocity.x = 350;
+            } else if (this.cursor.left.isDown && isShooting == false) { 
+                //if the left arrow is pressed, move to the left
+                this.mc.body.velocity.x = -350;
+            } else { //if no arrow keys are being pressed, stop moving
+                this.mc.body.velocity.x = 0;
+            }
+        }    
+    },
     
+    hit: function(hoop, mc) {
+        this.mc.x = 50;
+        this.mc.y = 4308;
+        isShooting = false;
+
+    }
 };
